21f8a061fa79d109c61202fe52eae8af57dbbc49
dialite <raymondolisa01@gmail.com>
2023-06-16 01:32:46 +0100

    Define scroll to top in browser router and structure file layout of components

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,27 @@
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import Home from "./scenes/home/Home";
+
+// Provided for react-router that enables starting at the top of new page
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
+
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ScrollToTop />
+        <Routes path="/" element={<Home />} />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;

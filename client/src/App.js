0853544f8deaf08b00b6e4d2f95b17ef10ca22d0
dialite <raymondolisa01@gmail.com>
2023-06-16 01:48:10 +0100

    Define route for each component in the App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import { useEffect } from "react";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Home from "./scenes/home/Home";
+import ItemDetails from "./scenes/itemDetails/ItemDetails";
+import Checkout from "./scenes/checkout/Checkout";
+import Confirmation from "./scenes/checkout/Confirmation";
 
 // Provided for react-router that enables starting at the top of new page
 const ScrollToTop = () => {
@@ -18,7 +21,12 @@ function App() {
     <div className="app">
       <BrowserRouter>
         <ScrollToTop />
-        <Routes path="/" element={<Home />} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="item/:itemId" element={<ItemDetails />} />
+          <Route path="checkout" element={<Checkout />} />
+          <Route path="checkout/success" element={<Confirmation />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );

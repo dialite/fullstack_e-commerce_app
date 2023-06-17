4e8a5537494fc9a57b0c9e4d64ee4c0e35eeebc8
dialite <raymondolisa01@gmail.com>
2023-06-17 14:55:36 +0100

    Correct error flaged for columnGap in navbar.jsx

--- client/src/scenes/global/Navbar.jsx
+++ client/src/scenes/global/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar = () => {
         <Box
           display="flex"
           justifyContent="space-between"
-          columnsGap="20px"
+          columnGap="20px"
           zIndex="2"
         >
           <IconButton sx={{ color: "black" }}>
@@ -54,12 +54,28 @@ const Navbar = () => {
           <IconButton sx={{ color: "black" }}>
             <PersonOutline />
           </IconButton>
-          <IconButton
-            onClick={() => dispatch(setIsCartOpen({}))}
-            sx={{ color: "black" }}
+
+          <Badge
+            badgeContent={cart.length}
+            color="secondary"
+            invisible={cart.length === 0}
+            sx={{
+              "& .MuiBadge-badge": {
+                right: 5,
+                top: 5,
+                padding: "0 4px",
+                height: "14px",
+                minWidth: "13px",
+              },
+            }}
           >
-            <ShoppingBagOutlined />
-          </IconButton>
+            <IconButton
+              onClick={() => dispatch(setIsCartOpen({}))}
+              sx={{ color: "black" }}
+            >
+              <ShoppingBagOutlined />
+            </IconButton>
+          </Badge>
           <IconButton sx={{ color: "black" }}>
             <MenuOutlined />
           </IconButton>

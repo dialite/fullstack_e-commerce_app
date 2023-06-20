cb3b6eb2aea565c9a59c4d5a38c1fdc54deb5647
dialite <raymondolisa01@gmail.com>
2023-06-20 18:57:40 +0100

    Import all required features for teh shopping list scenes

--- client/src/scenes/home/ShoppingList.jsx
+++ client/src/scenes/home/ShoppingList.jsx
@@ -1,2 +1,8 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Box, Typography, Tab, Tabs, useMediaQuery } from "@mui/material";
+import Item from "../../components/Item";
+import { setItems } from "../../state";
+
 const ShoppingList = () => {};
 export default ShoppingList;

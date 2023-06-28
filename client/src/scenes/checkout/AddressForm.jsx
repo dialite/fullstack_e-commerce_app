70d061be041b2be1956b3116b574d9f9aa29933b
dialite <raymondolisa01@gmail.com>
2023-06-28 20:59:43 +0100

    Modify styling zipcode and state fields in address form

--- client/src/scenes/checkout/AddressForm.jsx
+++ client/src/scenes/checkout/AddressForm.jsx
@@ -118,7 +118,7 @@ const AddressForm = ({
         name={formattedName("state")}
         error={formattedError("state")}
         helperText={formattedHelper("state")}
-        sx={{ gridColumn: "span 2" }}
+        sx={{ gridColumn: "1fr" }}
       />
 
       <TextField
@@ -131,7 +131,7 @@ const AddressForm = ({
         name={formattedName("zipCode")}
         error={formattedError("zipCode")}
         helperText={formattedHelper("zipCode")}
-        sx={{ gridColumn: "span 2" }}
+        sx={{ gridColumn: "1fr" }}
       />
     </Box>
   );

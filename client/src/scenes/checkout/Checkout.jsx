dde041b4286717a15fd1180b5b133c076c3d6a48
dialite <raymondolisa01@gmail.com>
2023-06-28 16:53:16 +0100

    Initialize checkout page with import of features

--- client/src/scenes/checkout/Checkout.jsx
+++ client/src/scenes/checkout/Checkout.jsx
@@ -1,4 +1,12 @@
+import { useSelector } from "react-redux";
+import { Box, Button, Stepper, Step, StepLabel } from "@mui/material";
+import { Formik } from "formik";
+import { useState } from "react";
+import * as yup from "yup";
+import { shades } from "../../theme";
+
 const Checkout = () => {
+  const [activeStep, setActiveStep] = useState(0);
   return <div>Checkout</div>;
 };
 
